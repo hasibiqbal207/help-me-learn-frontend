08a93228bfbf1fb3c4d34f8c815f28f6aae89f93
Hasib Iqbal <hasibiqbal207@gmail.com>
2024-10-27 23:44:50 +0100

    Backend replaced with the boilerplate, and tested with previous apis.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,19 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
-dotenv.config();
+import { fileURLToPath } from 'url';
+import path from "path";
+
+// dotenv.config();
+
+// Determine the environment and load the corresponding .env file
+const env = process.env.NODE_ENV || "development";
+const envFile = `.env.${env}`;
+// Convert `import.meta.url` to a file path
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name of the current module
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "..", envFile) });
 
 const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
